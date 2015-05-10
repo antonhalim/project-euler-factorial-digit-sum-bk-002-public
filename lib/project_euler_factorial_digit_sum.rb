0009df9f9b778214e8ce695f41ac34806560b7ed
axton21 <axton_n@yahoo.com>
2015-05-10 13:58:10 -0400

    its hard to understand what the questions wants =(

--- lib/project_euler_factorial_digit_sum.rb
+++ lib/project_euler_factorial_digit_sum.rb
@@ -1,1 +1,19 @@
-# code your solution here
+require 'pry'
+def factorial(num)
+	number = 1
+	for i in 1..num
+		number *= i
+	end
+	number
+end
+
+
+def sum_of_digits(num)
+	number = 0
+	num.to_s.split("").each{|x| number +=x.to_i}
+	number
+end
+
+def factorial_digit_sum(num)
+	sum_of_digits(factorial(num))
+end
